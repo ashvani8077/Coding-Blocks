9400fd89e41118272cfbe5b9566c3ff8fe52a7bf
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-03-15 16:50:24 +0530

    Add files via upload

---
+++ Arithmetic_Operator.java
@@ -0,0 +1,20 @@
+public class Arithmetic_Operator {
+    public static void main(String[] args) {
+        int a=20;
+        int b=4;
+        int sum=a+b;
+        System.out.println(sum);
+        int sub=a-b;
+        System.out.println(sub);
+        int mul=a*b;
+        System.out.println(mul);
+        int div=a/b;
+        System.out.println(div);
+        int mod=a%b;
+        System.out.println(mod);
+        int inc=++a;
+        System.out.println(inc);
+        int dec=--b;
+        System.out.println(dec);
+    }
+}
---
+++ Assignment_Operator.java
@@ -0,0 +1,25 @@
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int x=9;
+        x += 5;         //x=x+5
+        System.out.println(x);
+        x -=3;          //x=x-5
+        System.out.println(x);
+        x *=2;          //x=x*2
+        System.out.println(x);
+        x /=3;          //x=x/3
+        System.out.println(x);
+        x %= 2;         //x=x%2
+        System.out.println(x);
+        x &=5;          //x=x&5
+        System.out.println(x);
+        x |=2;          //x=x|2
+        System.out.println(x);
+        x ^= 4;         //x=x^4
+        System.out.println(x);
+        x >>=2;         //x=x>>2
+        System.out.println(x);
+        x <<=2;         //x=x<<2;
+        System.out.println(x);
+    }
+}
