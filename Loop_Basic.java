67cd2b6c4d8bbfa0206c734abf4ad4c66b5d8c7e
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-03-17 09:51:19 +0530

    Add files via upload

---
+++ Loop_Basic.java
@@ -0,0 +1,10 @@
+public class Loop_Basic {
+    public static void main(String[] args) {
+        int n=5;
+        int i=1;
+        while (i<=n){
+            System.out.println("Hello");
+            i++;
+        }
+    }
+}
