24474531cb446f8e97be809bfbb8e65f6d324cc4
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-03-17 09:52:03 +0530

    Add files via upload

---
+++ Grade_Card.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Grade_Card {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num= sc.nextInt();
+        if(num>90){
+            System.out.println("O");
+        }
+        if(num<91&&num>80){
+            System.out.println("A");
+        }
+        if(num<81&&num>70){
+            System.out.println("B");
+        }
+        if(num<71&&num>60){
+            System.out.println("C");
+        }
+        else {
+            System.out.println("Fail");
+        }
+    }
+}
