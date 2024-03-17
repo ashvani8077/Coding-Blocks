0230998c57da9e8357330752cb0f1297043dcfc2
ashvani8077 <panditashuabvp@gmail.com>
2024-03-17 21:21:56 +0530

    India

--- Grade_Card.java
+++ Grade_Card.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Grade_Card {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int num= sc.nextInt();
+        Scanner scan=new Scanner(System.in);
+        int num= scan.nextInt();
         if(num>90){
             System.out.println("O");
         }
